1780714a76b24a2f1a26c0894dfa1b652fc9897c
Tam T. Nguyen <nthanhtam@gmail.com>
2019-03-26 21:00:28 -0500

    fix: use nohup

--- start.sh
+++ start.sh
@@ -1,2 +1,1 @@
-screen -S jupyter
-jupyter notebook --NotebookApp.token= --NotebookApp.ip=*  --NotebookApp.port=8888 --NotebookApp.open_browser=False
+nohup jupyter notebook --NotebookApp.token= --NotebookApp.ip=*  --NotebookApp.port=8888 --NotebookApp.open_browser=False &
