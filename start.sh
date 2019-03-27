aa3b86b74a2b4223061961f63d7cd2a64dda7940
Tam T. Nguyen <nthanhtam@gmail.com>
2019-03-26 20:26:49 -0500

    update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,2 @@
-jupyter notebook --generate-config
-
-cd ~
-mkdir certs
-cd certs
-certdir=$(pwd)
-openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.key -out mycert.pem
-
-cd ~
-sed -i "1 a\
-c = get_config()\\
-c.NotebookApp.certfile = u'$certdir/mycert.pem'\\
-c.NotebookApp.keyfile = u'$certdir/mycert.key'\\
-c.NotebookApp.ip = '*'\\
-c.NotebookApp.open_browser = False\\
-c.NotebookApp.port = 8888" .jupyter/jupyter_notebook_config.py
-
 screen -S jupyter
-jupyter notebook --NotebookApp.token=
+jupyter notebook --NotebookApp.token= --NotebookApp.ip=*  --NotebookApp.port=8888 --NotebookApp.open_browser=False
