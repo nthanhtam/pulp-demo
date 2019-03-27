cddf525b3f2d7562e0f1cef1dd0b445af983e789
Tam T. Nguyen <nthanhtam@gmail.com>
2019-03-26 22:19:42 -0500

    feat: save result

--- qp_main.py
+++ qp_main.py
@@ -22,4 +22,6 @@ q = -dot(array([3., 2., 3.]), M).reshape((3,))
 G = array([[1., 2., 1.], [2., 0., 1.], [-1., 2., -1.]])
 h = array([3., 2., -2.]).reshape((3,))
 
-print(quadprog_solve_qp(P, q, G, h))
+result = quadprog_solve_qp(P, q, G, h)
+numpy.savetxt('./result.txt', result)
+print(result)
